252183cdbd4f4c689e70dde069ab73609462bb0a
JeffreyWebDev <68449168+JeffreyWebDev@users.noreply.github.com>
2021-02-15 23:59:06 -0500

    Submit daily screening form

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ browser_option.add_argument("headless")
 driver = webdriver.Chrome(PATH, options=browser_option)
 driver.get("https://veoci.com/veoci/134376/processes/236235613/invocations")
 
+
+def submit_daily_screening():
+    daily_screening_submit_btn = driver.find_elements_by_xpath(
+        '//*[@id="app"]/div/main/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/button'
+    )[0]
+    daily_screening_submit_btn.click()
+
+
 # Complete FGCU sign in form
 def fgcu_sign_in_page(email, password):
     fgcu_email_input_element = driver.find_element_by_id("branding-username")
@@ -24,6 +32,10 @@ def fgcu_sign_in_page(email, password):
 
     fgcu_submit_btn = driver.find_element_by_id("branding-sumbit-button")
     fgcu_submit_btn.click()
+
+    sleep(20)
+
+    submit_daily_screening()
 
 
 # Complete Veoci sign in form
