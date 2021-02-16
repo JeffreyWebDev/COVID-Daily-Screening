eac77ab29efc8e78170ee75ddad450fd379e0a97
JeffreyWebDev <68449168+JeffreyWebDev@users.noreply.github.com>
2021-02-15 23:29:01 -0500

    Initialize webdriver option and launch beginning webpage

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
 from time import sleep
 
+# Path to browser webdriver
+PATH = "INSERT BROWSER WEBDRIVER PATH"
+
+# Allow browser to automate task without window pop-up
+browser_option = webdriver.ChromeOptions()
+browser_option.add_argument("headless")
+
+# Launch starting webpage
+driver = webdriver.Chrome(PATH, options=browser_option)
+driver.get("https://veoci.com/veoci/134376/processes/236235613/invocations")
