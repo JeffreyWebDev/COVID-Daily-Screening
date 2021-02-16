1ea70a98c1c098b7e0fc44c17cd52cde76efe6ea
JeffreyWebDev <68449168+JeffreyWebDev@users.noreply.github.com>
2021-02-15 23:42:54 -0500

    Fill in the veoci sign in form

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ browser_option.add_argument("headless")
 # Launch starting webpage
 driver = webdriver.Chrome(PATH, options=browser_option)
 driver.get("https://veoci.com/veoci/134376/processes/236235613/invocations")
+
+# Complete Veoci sign in form
+def veoci_sign_in_page(email):
+    veoci_email_input_element = driver.find_element_by_id("j_username")
+    veoci_email_input_element.clear()
+    veoci_email_input_element.send_keys(email)
+
+    veoci_form_submit_btn = driver.find_element_by_xpath(
+        '//*[@id="veoci-password-login"]/div/div[1]/input[2]'
+    )
+    veoci_form_submit_btn.click()
+
+    return sleep(3)
+
+
+veoci_sign_in_page("ENTER VEOCI SIGN EMAIL")
