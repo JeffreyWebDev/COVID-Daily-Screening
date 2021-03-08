867aad02dfdec72836dc7879ec09154d15d034fd
JeffreyWebDev <68449168+JeffreyWebDev@users.noreply.github.com>
2021-03-07 19:12:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from selenium import webdriver
 import socket
 from time import sleep
 
-# Path to browser webdriver
-PATH = "INSERT BROWSER WEBDRIVER PATH"
+driver = None
 
-# Allow browser to automate task without window pop-up
-browser_option = webdriver.ChromeOptions()
-browser_option.add_argument("headless")
+webdriver_path = input("Please enter your webdriver path: ")
 
-# Launch starting webpage
-driver = webdriver.Chrome(PATH, options=browser_option)
-driver.get("https://veoci.com/veoci/134376/processes/236235613/invocations")
+while driver is None:
+
+    try:
+        # Path to browser webdriver
+        PATH = webdriver_path
+
+        # Allow browser to automate task without window pop-up
+        browser_option = webdriver.ChromeOptions()
+        # browser_option.add_argument("headless")
+
+        # Launch starting webpage
+        driver = webdriver.Chrome(PATH, options=browser_option)
+        driver.get("https://veoci.com/veoci/134376/processes/236235613/invocations")
+    except Exception as err:
+        print(
+            "Path not found, visit https://sites.google.com/a/chromium.org/chromedriver/home for more information"
+        )
+        webdriver_path = input("Please enter your webdriver path: ")
+        driver = None
 
 
 def submit_daily_screening():
@@ -45,14 +58,39 @@ def fgcu_sign_in_page(email, password):
     return submit_daily_screening()
 
 
-def veoci_sign_in_page(email):
+def veoci_sign_in_page():
+    veoci_email = input("Please input Veoci email: ")
+
     veoci_email_input_element = driver.find_element_by_id("j_username")
-    fill_input_element(veoci_email_input_element, email)
+    fill_input_element(veoci_email_input_element, veoci_email)
 
     veoci_form_submit_btn = driver.find_element_by_xpath(
         '//*[@id="veoci-password-login"]/div/div[1]/input[2]'
     )
     veoci_form_submit_btn.click()
+
+    sleep(2)
+
+    invalid_email = driver.find_element_by_xpath(
+        '//*[@id="login-user-summary"]/div[2]/strong'
+    )
+
+    while invalid_email:
+        veoci_email = input("Please input Veoci email: ")
+
+        veoci_email_input_element = driver.find_element_by_id("j_username")
+        fill_input_element(veoci_email_input_element, veoci_email)
+
+        veoci_form_submit_btn = driver.find_element_by_xpath(
+            '//*[@id="veoci-password-login"]/div/div[1]/input[2]'
+        )
+        veoci_form_submit_btn.click()
+
+        sleep(2)
+
+        invalid_email = driver.find_element_by_xpath(
+            '//*[@id="login-user-summary"]/div[2]/strong'
+        )
 
     fgcu_sign_in_email = input("FGCU login email: ")
     fgcu_sign_in_password = input("FGCU login password: ")
@@ -66,6 +104,5 @@ if IPaddress == "127.0.0.1":
     print("No Internet connection")
     quit()
 else:
-    veoci_email = input("Please input Veoci email: ")
-    veoci_sign_in_page(veoci_email)
+    veoci_sign_in_page()
 
